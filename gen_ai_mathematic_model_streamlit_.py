394e8f5a1d460f41b8275929066b837ccbc00ddf
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-05 18:46:15 +0530

    Update gen_ai_mathematic_model_streamlit_.py

--- gen_ai_mathematic_model_streamlit_.py
+++ gen_ai_mathematic_model_streamlit_.py
@@ -7,12 +7,12 @@ Original file is located at
     https://colab.research.google.com/drive/1pvx1L7VwAWTZ-D8EDzd58OUgiOcgbZq2
 """
 
-import streamlit as st
-import sympy as sp
-import numpy as np
-from scipy.fft import fft
-from scipy.integrate import quad
-from sympy import Matrix, symbols, diff, integrate, laplace_transform
+ ###import streamlit as st
+ ##import sympy as sp
+##import numpy as np
+##from scipy.fft import fft
+##from scipy.integrate import quad
+##from sympy import Matrix, symbols, diff, integrate, laplace_transform
 
 st.set_page_config(page_title="Advanced Math Solver", layout="wide")
 
